0b7b51008bafa29d5a77592b2995edca64087d19
Nicolas Vasquez <nvasquez682@gmail.com>
2020-03-06 14:01:19 -0800

    Added Health script and NickTest Scene

---
+++ OneForAllProject/Assets/Scripts/CollisionKey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionKey : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ OneForAllProject/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    private float curHealth;
+    public GameObject self;
+
+    void Start()
+    {
+        curHealth = maxHealth;   
+    }
+
+   
+    void Update()
+    {
+        if (curHealth >= maxHealth) //Exceed Max HP
+            curHealth = maxHealth;
+
+        if (curHealth <= 0) //Death
+            self.SetActive(false);
+            /* Will probably eventually add conditions that disable
+               the activation of hero that is dead and everything else tied to them   */
+    }
+
+    void DecreaseHealth(float dmg)
+    {
+        curHealth = curHealth - dmg;
+    }
+}
