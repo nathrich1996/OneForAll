610cfe25250181f9fffc369d0a542a076d8e13c0
Luis Rangel <xxcmgxx02@gmail.com>
2020-03-06 21:10:24 -0800

    Added PlayerController Script
    Basic Horizontal Movement

---
+++ OneForAllProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+{
+    private Rigidbody2D RigidBody;
+    public float moveSpeed = 5f;
+    float HorizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        Vector3 movement = new Vector3(HorizontalMove, 0f, 0f);
+        transform.position += movement * Time.deltaTime;
+    }
+}
