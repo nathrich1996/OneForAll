c95d8a0f881aee139dfb6c768e8179d9993b5bbf
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2020-03-10 23:48:54 -0700

    Added Functionality to Actvate Abilities and Swap Heroes
    Will test tomorrow

---
+++ OneForAllProject/Assets/Scripts/Player/ActivateAbility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player.HeroAbility;
+
+public class ActivateAbility : MonoBehaviour
+{
+     HeroSwap hs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hs = gameObject.GetComponent<HeroSwap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        CheckForSwap();
+        ChecktoActivateAbility();
+    }
+    void ChecktoActivateAbility()
+    {
+        if (Input.GetKeyDown("Q"))
+        {
+            hs.currentSet.ActivateFirstAbility();
+        }
+        else if (Input.GetKeyDown("E"))
+        {
+            hs.currentSet.ActivateSecondAbility();
+        }
+    }
+    void CheckForSwap()
+    {
+        if (Input.GetKeyDown("1") && hs.currentHero != "Justice") //Activate Old Justice
+        {
+            hs.SwitchAbility("Justice");
+        }
+        else if (Input.GetKeyDown("2") && hs.currentHero != "Atlas") //Activate Atlas
+        {
+            hs.SwitchAbility("Atlas");
+        }
+        else if (Input.GetKeyDown("3") && hs.currentHero != "Calico") //Activate Calico 
+        {
+            hs.SwitchAbility("Calico");
+        }
+    }
+}
