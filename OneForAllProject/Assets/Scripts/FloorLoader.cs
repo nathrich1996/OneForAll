02b406ac7a19a1910b6ac2d9a182db5e21aa22d6
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2020-04-27 03:04:20 -0700

    Hero Puzzles Load Floors Separately (Changed Build Settings to Load Post MVP)
    Also Added Game Over and AFO Puzzle Scenes to get started

---
+++ OneForAllProject/Assets/Scripts/FloorLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLoader : MonoBehaviour //Loads a section of level at time to hopefully get faster loading
+{
+    public GameObject floorToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorToLoad.SetActive(false); //don't spawn at start
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag =="Player")//if player walks in
+        {
+            floorToLoad.SetActive(true); //load section
+            Debug.Log("Floor loaded: " + floorToLoad.name);
+        }
+    }
+}
