64f418d84cb81e316e3430782c08fe1bffee01b6
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2020-04-28 00:57:21 -0700

    Added in Ability Logos, HUD Shows Ability Logos As Well
    Also I think i forgot to put the sound folder in last minute

---
+++ OneForAllProject/Assets/Scripts/UI/AbilityLogos.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityLogos : MonoBehaviour
+{
+    public Image currentAbility1;
+    public Image currentAbility2;
+
+    public Sprite ojAbility1;
+    public Sprite ojAbility2;
+
+    public Sprite atlasAbility1;
+    public Sprite atlasAbility2;
+
+    public Sprite calicoAbility1;
+    public Sprite calicoAbility2;
+    // Start is called before the first frame update
+    HeroSwap hs;
+    string currentHero;
+    string swappedHero;
+    void Start()
+    {
+        hs = GameObject.FindGameObjectWithTag("Player").GetComponent<HeroSwap>();
+        currentHero = hs.GetCurrentHero();
+        string swappedHero = currentHero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHero = hs.GetCurrentHero();
+        if (currentHero!=swappedHero)
+        {
+            swappedHero = currentHero;
+            ChangeAbilityLogos(swappedHero);
+        }
+    }
+    void ChangeAbilityLogos(string hero)
+    {
+        switch(hero)
+        {
+            case "Justice":
+                currentAbility1.sprite = ojAbility1;
+                currentAbility2.sprite = ojAbility2;
+                break;
+            case "Atlas":
+                currentAbility1.sprite = atlasAbility1;
+                currentAbility2.sprite = atlasAbility2;
+                break;
+            case "Calico":
+                currentAbility1.sprite = calicoAbility1;
+                currentAbility2.sprite = calicoAbility2;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ OneForAllProject/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadAristides()
+    {
+        //SceneManager.LoadScene();
+    }
+    public void LoadHeroPuzzle()
+    {
+        //SceneManager.LoadScene();
+    }
+    public void LoadAFOPuzzle()
+    {
+        //SceneManager.LoadScne();
+    }
+}
