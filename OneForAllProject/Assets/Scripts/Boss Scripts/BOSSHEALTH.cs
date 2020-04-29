a5b5e20046bf630ababbcb698e23a7e959778aea
Nicolas Vasquez <nvasquez682@gmail.com>
2020-04-29 00:15:31 -0700

    BossHealth + Melee Hitbox

---
+++ OneForAllProject/Assets/Scripts/Boss Scripts/BOSSHEALTH.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BOSSHEALTH : MonoBehaviour
+{
+    public float maxHealth;
+    public float curHealth;
+
+    public Slider healthBar;
+
+    void Start()
+    {
+        maxHealth = 300f;
+        curHealth = maxHealth;
+        healthBar.value = CalcHealth();
+    }
+
+
+    void Update()
+    {
+        if (curHealth >= maxHealth) //Exceed Max HP
+            curHealth = maxHealth;
+
+        if (curHealth <= 0)
+        {
+           // SceneManager.LoadScene(/*"SceneName*/);
+        }
+
+    }
+
+    public void DecreaseHealth(float dmg)
+    {
+        curHealth = curHealth - dmg;
+
+        Debug.Log("- " + dmg + "HP");
+        Debug.Log("HP: " + curHealth);
+
+        healthBar.value = CalcHealth();
+
+    }
+
+    public float CalcHealth()
+    {
+        return curHealth;
+    }
+}
---
+++ OneForAllProject/Assets/Scripts/MELEE_HB_CONd.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MELEE_HB_CONd : MonoBehaviour
+{
+    public HeroSwap hs = GameObject.FindGameObjectWithTag("Player").GetComponent<HeroSwap>();
+    string hero;
+    float damage;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        hero = hs.GetCurrentHero();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (hero == "Atlas" && collision.gameObject.tag == "Boss")
+        {
+            damage = 40f;
+            collision.gameObject.GetComponent<BOSSHEALTH>().DecreaseHealth(damage);
+        }
+
+        if (hero == "Justice" && collision.gameObject.tag == "Boss")
+        {
+            damage = 20f;
+            collision.gameObject.GetComponent<BOSSHEALTH>().DecreaseHealth(damage);
+        }
+
+        if (hero == "Calico" && collision.gameObject.tag == "Boss")
+        {
+            damage = 15f;
+            collision.gameObject.GetComponent<BOSSHEALTH>().DecreaseHealth(damage);
+        }
+    }
+}
