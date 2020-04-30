5355593de1c21ad7eb83e714343a699973e0a751
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2020-04-30 16:42:33 -0700

    How To Play and Updated Credits (Buil Settings change)
    Put Music and edited changes in credits. Main Menu can load into both and both load into Main Menu

--- OneForAllProject/Assets/Scripts/UI/AbilityLogos.cs
+++ OneForAllProject/Assets/Scripts/UI/AbilityLogos.cs
@@ -7,15 +7,19 @@ public class AbilityLogos : MonoBehaviour
 {
     public Image currentAbility1;
     public Image currentAbility2;
+    public Image currentHead;
 
     public Sprite ojAbility1;
     public Sprite ojAbility2;
+    public Sprite ojHead;
 
     public Sprite atlasAbility1;
     public Sprite atlasAbility2;
+    public Sprite atlasHead;
 
     public Sprite calicoAbility1;
     public Sprite calicoAbility2;
+    public Sprite calicoHead;
     // Start is called before the first frame update
     HeroSwap hs;
     string currentHero;
@@ -44,14 +48,17 @@ public class AbilityLogos : MonoBehaviour
             case "Justice":
                 currentAbility1.sprite = ojAbility1;
                 currentAbility2.sprite = ojAbility2;
+                currentHead.sprite = ojHead;
                 break;
             case "Atlas":
                 currentAbility1.sprite = atlasAbility1;
                 currentAbility2.sprite = atlasAbility2;
+                currentHead.sprite = atlasHead;
                 break;
             case "Calico":
                 currentAbility1.sprite = calicoAbility1;
                 currentAbility2.sprite = calicoAbility2;
+                currentHead.sprite = calicoHead;
                 break;
             default:
                 break;
