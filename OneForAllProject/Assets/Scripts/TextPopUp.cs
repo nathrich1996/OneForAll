851f9b9d5c4008354be77a5dbcc6f33e5715b7b8
Luis Rangel <xxcmgxx02@gmail.com>
2020-06-09 11:31:13 -0700

    UI Text Pop Up
    Added TextPopUp and TextPopUpTrigger scripts.

---
+++ OneForAllProject/Assets/Scripts/TextPopUp.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*dialogue text box is attached to the canvas and disabled by default, and enabled when needed.*/
+public class TextPopUp : MonoBehaviour
+{
+    //set in inspector
+    public GameObject TextBoxPopUp;
+    public GameObject Dialogue;
+    private Text PopUpTxt;
+    public Image CharPortrait;
+
+    public Sprite charSprite;
+    public string txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //////////
+        /////////
+        PopUpTxt = Dialogue.GetComponent<Text>();
+        /////////
+
+        //Initialize in inspector (optional)
+        PopUpTxt.text = string.Copy(txt);
+        CharPortrait.sprite = charSprite;
+
+        //Hide PopUp
+        TextBoxPopUp.GetComponent<Image>().canvasRenderer.SetAlpha(0f);
+        PopUpTxt.canvasRenderer.SetAlpha(0f);
+        CharPortrait.canvasRenderer.SetAlpha(0f);
+    }
+
+    public void SetSprite(Sprite SpriteImg)
+    {
+        CharPortrait.sprite = SpriteImg;
+    }
+
+    public void SetText(string txt)
+    {
+        PopUpTxt.text = string.Copy(txt);
+       
+    }
+
+    public void FadeIn()
+    {
+        TextBoxPopUp.GetComponent<Image>().CrossFadeAlpha(1.0f, 1.5f, false);
+        PopUpTxt.CrossFadeAlpha(1.0f, 1.5f, false);
+        CharPortrait.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    public void FadeOut()
+    {
+        TextBoxPopUp.GetComponent<Image>().CrossFadeAlpha(0f, 1.5f, false);
+        PopUpTxt.CrossFadeAlpha(0f, 1.5f, false);
+        CharPortrait.CrossFadeAlpha(0f, 1.5f, false);
+    }
+}
---
+++ OneForAllProject/Assets/Scripts/TextPopUpTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*dialogue text box is attached to the canvas and disabled by default, and enabled when needed.*/
+public class TextPopUpTrigger : MonoBehaviour
+{
+
+    public TextPopUp PopUp;
+
+    void Start()
+    {
+        //PopUp = FindObjectOfType<TextPopUp>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag =="Player")
+        {
+            //Debug.Log("Trigger entered.");
+            PopUp.FadeIn();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Trigger exited");
+            PopUp.FadeOut();
+        }
+    }
+}
